0ff0850692f9e537372d4515c60b804bb944b63a
kagamiyizimi <1148420498@qq.com>
2017-09-28 11:24:39 +0800

    20170928

---
+++ runtime/temp/84dc1dfafee7aa17ab5fda7e2f46db8c.php
@@ -0,0 +1,52 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:78:"D:\php_project\test2\emiya\public/../application/index\view\widget\footer.html";i:1506513041;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link href="__STATIC__/index/css/footer.css" rel="stylesheet">
+</head>
+<body>
+<!--底部内容-->
+<div class="footer">
+     <img src="__STATIC__/index/images/footer_pic-small.jpg" style="width:100%;height: 880px">
+    <div class="footerMain">
+     <div class="footerMain_left">
+         <img src="__STATIC__/index/images/logo_v.png" style="width: 120px;height: 150px;float: left">
+         <p>我们希望从每个家庭餐桌上的最小单位一粒米开始，以线上市集的方法，让更多人结识友善耕耘的农夫，一步一步、一米一米地改善全中国的饮食生态。</p>
+     </div>
+     <div class="footerMain_center">
+         <ul>
+             <li><a href="#">相识一米市集</a></li>
+             <li><a href="#">邀约农友</a></li>
+             <li><a href="#">退换货办法</a></li>
+             <li><a href="#">常遇问题</a></li>
+             <li><a href="#">会员积分及感谢制度</a></li>
+         </ul>
+         <ul>
+             <li><a href="#">全程冷链</a></li>
+             <li><a href="#">送达范围及时段</a></li>
+             <li><a href="#">运费提示</a></li>
+         </ul>
+         <ul>
+             <li><a href="#">发票提示</a></li>
+             <li><a href="#">优惠券用法</a></li>
+             <li><a href="#">支付方式</a></li>
+         </ul>
+     </div>
+     <div class="footerMain_right">
+             <p>服务热线</p><p class="number">400-655-1212
+         </p><p>
+         </p><p>周一至周日</p>
+             <p class="number">8:00-21:00
+             </p><p>
+         </p><p>微信公众号</p>
+         <img src="__STATIC__/index/images/qr.png" style="width: 90px;height: 90px;margin-top: 20px">
+        </div>
+    </div>
+    <div class="footerBottom">
+        沪ICP备15037721号 | 食品流通许可证SP3101051510004275 | 上海市长宁区天山西路789号中山国际广场B座2层 | © Copyright 2015 保留所有权利
+    </div>
+</div>
+</body>
+</html>
